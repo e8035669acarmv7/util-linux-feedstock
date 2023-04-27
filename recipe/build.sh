59d13fa5dd02b2267c430d6a6f09b10a6ebbc73f
jaimergp <jaimergp@users.noreply.github.com>
2023-04-27 14:52:12 +0200

    more flaky known fails

--- recipe/build.sh
+++ recipe/build.sh
@@ -14,8 +14,13 @@ set -ex
             --disable-makeinstall-setuid \
             --without-systemdsystemunitdir
 make -j ${CPU_COUNT}
-make check \
-  TS_OPT_misc_setarch_known_fail=yes \
-  TS_OPT_column_invalid_multibyte_known_fail=yes \
-  TS_OPT_hardlink_options_known_fail=yes  # flaky
+
+known_fail="TS_OPT_misc_setarch_known_fail=yes"
+known_fail+=" TS_OPT_column_invalid_multibyte_known_fail=yes"
+known_fail+=" TS_OPT_hardlink_options_known_fail=yes"  # flaky on py3.9?
+if [[ $target_platform == linux-aarch64 ]]; then
+  known_fail+=" TS_OPT_lsfd_mkfds_ro_regular_file_known_fail"  # can be flaky on this platform
+fi
+make check $known_fail
+
 make install
