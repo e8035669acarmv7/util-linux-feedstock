38f50f5c2cf9a6277afa2eb4d4209639fb14264e
Mark Harfouche <mark.harfouche@gmail.com>
2023-04-28 08:55:38 -0400

    do not copy over config

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 # Get an updated config.sub and config.guess
-cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
+# cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
 set -ex
 
 OSX_ARGS=""
