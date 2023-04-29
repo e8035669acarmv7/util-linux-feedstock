00a8dde668e4590e600f0980c0e1b9f19760cc68
Mark Harfouche <mark.harfouche@gmail.com>
2023-04-28 20:25:33 -0400

    remove config guess and autoconf

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-# Get an updated config.sub and config.guess
-# cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
 set -ex
 
 OSX_ARGS=""
@@ -15,8 +13,6 @@ if [[ $target_platform == "osx-"* ]]; then
             --enable-libuuid"
 
 fi
-
-# autoreconf -vfi
 
 ./configure --prefix="${PREFIX}" \
             --disable-chfn-chsh  \
