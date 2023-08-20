7575909a56d5fec5e6dc575e0921f68c2e1e452a
Mark Harfouche <mark.harfouche@gmail.com>
2023-08-20 13:27:57 -0400

    cpp flags

--- recipe/build.sh
+++ recipe/build.sh
@@ -16,7 +16,7 @@ fi
 
 # https://kernelnewbies.org/Linux_4.10
 # https://elixir.bootlin.com/linux/v4.10.17/source/include/uapi/linux/sockios.h
-MISSING_DEFINES=-DSIOCGSKNS=0x894C
+export CPPFLAGS="${CPPFLAGS} -DSIOCGSKNS=0x894C"
 
 ./configure --prefix="${PREFIX}" \
             --disable-chfn-chsh  \
@@ -31,7 +31,7 @@ MISSING_DEFINES=-DSIOCGSKNS=0x894C
             --disable-makeinstall-setuid \
             --without-systemdsystemunitdir \
             $OSX_ARGS
-make ${MISSING_DEFINES} -j ${CPU_COUNT}
+make -j ${CPU_COUNT}
 
 known_fail="TS_OPT_misc_setarch_known_fail=yes"
 known_fail+=" TS_OPT_column_invalid_multibyte_known_fail=yes"
@@ -40,7 +40,7 @@ if [[ $target_platform == linux-aarch64 ]]; then
   known_fail+=" TS_OPT_lsfd_mkfds_ro_regular_file_known_fail=yes"  # can be flaky on this platform
 fi
 if [[ "${CONDA_BUILD_CROSS_COMPILATION:-}" != "1" || "${CROSSCOMPILING_EMULATOR}" != "" ]]; then
-make check ${MISSING_DEFINES} $known_fail
+make check $known_fail
 fi
 
 make install
